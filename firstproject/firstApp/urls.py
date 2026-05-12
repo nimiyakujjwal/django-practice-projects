86b14db87d7c0cdf3d8ca40aa84c6b434b055e28
Ujjwal Kumar <ujjwal-kumar2>
2026-05-12 16:30:02 +0530

    Application level URLs
    	new file:   firstApp/urls.py
    	modified:   firstproject/urls.py
    	new file:   quoteApp/urls.py

--- firstproject/firstApp/urls.py
+++ firstproject/firstApp/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+from firstApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('firstApp/', include('firstApp.urls')),
-    path('quoteApp/', include('quoteApp.urls'))
+    path('hello/', views.display, name='hello'),
+    path('datetime/', views.displayDateTime, name='displayDateTime')
 ]
