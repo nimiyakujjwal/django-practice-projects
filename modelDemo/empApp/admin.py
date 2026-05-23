71369a4bf1f43fa9a650caf2d4767aefe611fc48
nimiyakujjwal <26951554+nimiyakujjwal@users.noreply.github.com>
2026-05-23 17:38:40 +0530

    Register Employee model in admin panel

--- modelDemo/empApp/admin.py
+++ modelDemo/empApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Employee
 
 # Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
+
+admin.site.register(Employee, EmployeeAdmin)
