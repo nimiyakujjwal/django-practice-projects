8b678442af729226b07d1e9636e235e6e604bfd7
nimiyakujjwal <26951554+nimiyakujjwal@users.noreply.github.com>
2026-05-23 17:10:06 +0530

    Set DEBUG True for static files load

--- modelDemo/modelDemo/settings.py
+++ modelDemo/modelDemo/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = True # os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
